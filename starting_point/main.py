dfd2ba2f235d6e6f02d9b0f986d4582bb2629812
pedrocunha <pedro.cunha@ine.pt>
2026-05-21 16:22:17 +0000

    maio21

--- starting_point/main.py
+++ starting_point/main.py
@@ -7,7 +7,7 @@
 from sklearn.datasets import make_regression
 import pandas as pd
 
-X, y = make_regression(n_samples=200, n_features=3, noise=15, random_state=42)
+X, y = make_regression(n_samples=300, n_features=3, noise=15, random_state=32)
 
 df = pd.DataFrame(X, columns=['X1', 'X2', 'X3'])
 df['y'] = y
