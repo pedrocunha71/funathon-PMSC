74a589ada725277c29f3b4fc00ea9ab1a5fdbfbe
pedrocunha <pedro.cunha@ine.pt>
2026-05-27 11:15:34 +0000

    1preprocessing_ex2

--- starting_point/1preprocessing.py
+++ starting_point/1preprocessing.py
@@ -24,3 +24,39 @@ trans.head(n=5) # first n rows
 trans.isnull().sum() # nb of NaN per column
 trans.notnull().all() # column with no NaN?
 # %%
+import pandas as pd
+
+trans = trans[trans["prop_loc_dep"].isin(["75", "77", "78", "91", "92", "93", "94", "95"])]
+trans.head(n=10)
+# %%
+trans["price_sqm"]=trans["price"]/trans["farea"]
+trans.head()
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = trans["price_sqm"]
+p = np.percentile(y, 99.5)
+
+fig, axes = plt.subplots(4, 1, figsize=(12, 12))
+
+for ax, (data, label) in zip(axes, [(y, "Y"), (y[y <= p], "Y filtered"), (np.log(y), "log(Y)"), (np.log(y[y <= p]), "log(Y) filtered")]):
+    ax.hist(data, bins="auto", edgecolor="white", color="#334887", alpha=0.5)
+    ax.set_title(label)
+    ax.set_xlabel("Price per square meter")
+    ax.set_ylabel("Number of transactions")
+
+plt.tight_layout()
+plt.show()
+# %%
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+for ax, (data, label) in zip(axes, [(y[y <= 2000], "Y below 2000€ per sqm"), (y[y <= 500], "Y below 500€ per sqm")]):
+    ax.hist(data, bins="auto", edgecolor="white", color="#334887", alpha=0.95)
+    ax.set_title(label)
+    ax.set_xlabel("Price per square meter")
+    ax.set_ylabel("Number of transactions")
+
+plt.tight_layout()
+plt.show()
+# %%
